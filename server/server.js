5c2b676066fb0d18aae871d30d721a4089647553
Jed Koops <jed@jkoops.com>
2026-04-14 21:14:28 -0400

    auto: sync project files

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ require('dotenv').config({ path: '../.env' });
 
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
